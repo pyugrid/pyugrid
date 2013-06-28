51a99c8d50374fd712201db683882047597948a4
chris.barker <chris.barker@noaa.gov>
2013-06-28 10:24:43 -0700

    Just the first few files

---
+++ pyugrid/ucube.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+Core class that handles a dataset on an unstructured grid.
+
+"""
+
+
+class UCube():
+    """
+    Core class that handles a dataset on an unstructured grid.
+    """
+
+    def __init__():
+        pass
+
+    
