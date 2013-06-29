d509bcc0a1755324c21f3def970262d0dedec845
Alexander Crosby <crosbyar@gmail.com>
2013-06-29 15:45:59 -0400

    working read method for cf ~v0.9ugrid, that just builds a dict of meshes availble in the file. each mesh object is represented by the ugrid class

--- pyugrid/uread.py
+++ pyugrid/uread.py
@@ -1,25 +1,31 @@
-import os, sys, netCDF4
+import netCDF4
 import numpy as np
-from pyugrid import ugrid.ugrid as ug
-
-def open_cf_todict( filename ):
+from ugrid import ugrid as ug
+
+def open_cf_todict( filename ):
     nc = netCDF4.Dataset(filename, 'r')
-    ncvars = nc.variables
+    ncvars = nc.variables
     meshes = {}
     for varname in ncvars.iterkeys():
-        meshname = ncvars[varname].getncattr('mesh')
-        if (meshname != None) or (meshname not in set(meshes.getkeys())):
-            meshatts = ncvars[meshname].getncatts()
+        try:
+            meshname = ncvars[varname].getncattr('mesh')
+        except AttributeError:
+            meshname = None
+        if (meshname != None) and (meshname not in set(meshes.viewkeys())):
+            meshatt_names = ncvars[meshname].ncattrs()
             
             ## Make sure that this mesh style is supported in this codebase
+            meshatts = {}
+            for attname in meshatt_names:
+                meshatts[attname] = ncvars[meshname].getncattr(attname)
             assert meshatts['cf_role'] == 'mesh_topology'
-            if meshatts['topology_dimension'] != '2':
-                raise error("Unfortuntely, only meshes that are unstructured in 2 dimensions are supported")
-     
+            #if meshatts['topology_dimension'] != '2':
+            #    raise ValueError("Unfortuntely, only meshes that are unstructured in 2 dimensions are supported")
+            
             ## Grab node coordinates from mesh meta-variable, and pull out the coord values
             node_coordinates = meshatts.get('node_coordinates', None)
             if node_coordinates == None:
-                raise error("Unstructured meshes must include node coordinates, specified with the 'node_coordinates' attribute")
+                raise AttributeError("Unstructured meshes must include node coordinates, specified with the 'node_coordinates' attribute")
             node_coordinates = node_coordinates.split(" ")
             nodes = [None, None]
             for coord in node_coordinates:
@@ -29,7 +35,7 @@ def open_cf_todict( filename ):
                 elif ('east' in units) or ('west' in units):
                     nodes[1] = ncvars[coord][:]
                 else:
-                    raise error("Node coordinates don't contain 'units' attribute!")  
+                    raise AttributeError("Node coordinates don't contain 'units' attribute!")  
             
             ## Grab Face and Edge node connectivity arrays
             face_node_conn_name = meshatts.get('face_node_connectivity', None)
@@ -38,9 +44,18 @@ def open_cf_todict( filename ):
             edges = []
 	    if face_node_conn_name != None:
                 faces = ncvars[face_node_conn_name][:,:]
-            if edge_node_conn_name != None:
-                edges = ncvars[edge_node_conn_name][:,:]
-                          
+                index_base = np.min(np.min(faces))
+                if index_base  >= 1:
+                    faces = faces - index_base
+            try:
+                if edge_node_conn_name != None:
+                    edges = ncvars[edge_node_conn_name][:,:]
+                    index_base = np.min(np.min(edges))
+                    if index_base >= 1:
+                        edges = edges - index_base
+            except:
+                pass #TODO: Generate edge node topology if none exists, perhaps optional
+  
             ## Add to dictionary of meshes
             meshes[meshname] = ug(nodes, faces, edges)
     return meshes # Return dictionary of ugrid objects
