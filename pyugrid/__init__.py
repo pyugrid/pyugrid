de02ea09c9d3566360fe23f9fc1944870ecab70a
ocefpaf <ocefpaf@gmail.com>
2016-02-16 15:16:01 -0300

    Rename io

--- pyugrid/__init__.py
+++ pyugrid/__init__.py
@@ -9,7 +9,8 @@ from __future__ import (absolute_import, division, print_function)
 
 from .ugrid import UGrid
 from .uvar import UVar
+from . import grid_io
 
 __version__ = '0.1.7'
 
-__all__ = ['UGrid', 'UVar']
+__all__ = ['UGrid', 'UVar', 'grid_io']
